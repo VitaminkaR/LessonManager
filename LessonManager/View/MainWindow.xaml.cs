7e8046d4ef8de8e85fb21059cd151b136b4ac646
VladimirR <rvitaminka20@gmail.com>
2025-01-12 18:54:20 +0300

    MVVM Pattern

--- LessonManager/View/MainWindow.xaml.cs
+++ LessonManager/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LessonManager
+namespace LessonManager.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
