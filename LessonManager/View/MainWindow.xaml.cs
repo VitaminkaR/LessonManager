33072cdbb1e0cf989fa2998e26975688548175f0
VladimirR <rvitaminka20@gmail.com>
2025-01-14 16:24:05 +0300

    Вывод семестра и в нем дисциплины

--- LessonManager/View/MainWindow.xaml.cs
+++ LessonManager/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using LessonManager.ViewModel;
+﻿using LessonManager.Model;
+using LessonManager.ViewModel;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,10 +19,43 @@ namespace LessonManager.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainViewModel m_ViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
+            m_ViewModel = new MainViewModel();
+            DataContext = m_ViewModel;
+
+            m_ViewModel.Subjects.CollectionChanged += Subjects_CollectionChanged;
+        }
+
+        private void Subjects_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            ObservableCollection<Subject> subjects = (ObservableCollection<Subject>)sender;
+            SubjectsAndLabTreeView.Items.Clear();
+            Dictionary<int, List<string>> semesterAndSubjects = new Dictionary<int, List<string>>();
+            for (int i = 0; i < subjects.Count; i++)
+            {
+                int sem = subjects[i].SemesterNumber;
+                if (!semesterAndSubjects.ContainsKey(sem)) semesterAndSubjects[sem] = new List<string>();
+                semesterAndSubjects[sem].Add(subjects[i].Name);
+            }
+
+            foreach (var key in semesterAndSubjects.Keys)
+            {
+                TreeViewItem treeViewItemSemesters = new TreeViewItem();
+                treeViewItemSemesters.Header = key.ToString();
+                List<string> list = semesterAndSubjects[key];
+                for (int i = 0; i < list.Count; i++)
+                {
+                    TreeViewItem treeViewItemSubjects = new TreeViewItem();
+                    treeViewItemSubjects.Header = list[i];
+                    treeViewItemSemesters.Items.Add(treeViewItemSubjects);
+                }
+                SubjectsAndLabTreeView.Items.Add(treeViewItemSemesters);
+            }
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
