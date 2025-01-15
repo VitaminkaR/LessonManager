69a0d6732ff03861dae1d84c3a87fba72b56d486
VladimirR <rvitaminka20@gmail.com>
2025-01-15 18:22:39 +0300

    Добавлен класс занятий

---
+++ LessonManager/Model/Activity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonManager.Model
+{
+    enum ActivityType
+    {
+        Lab,
+        Prac,
+        Lec
+    }
+
+    enum ActivityStateType
+    {
+        None,
+        Ready,
+        Passed,
+        Visited,
+        NotVisited,
+        Answer
+    }
+
+    internal class Activity
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public ActivityType Type { get; set; }
+        public DateTime ActivityTime { get; set; }
+        public ActivityStateType State { get; set; }
+
+        public Activity(int iD, string? name, ActivityType type, DateTime activityTime, ActivityStateType state)
+        {
+            ID = iD;
+            Name = name;
+            Type = type;
+            ActivityTime = activityTime;
+            State = state;
+        }
+    }
+}
