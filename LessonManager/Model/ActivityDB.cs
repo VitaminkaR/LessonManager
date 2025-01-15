f13b780b1b515097314424c912d770e6dd939bc2
VladimirR <rvitaminka20@gmail.com>
2025-01-15 19:04:07 +0300

    Взаимодействие занятий с БД

---
+++ LessonManager/Model/ActivityDB.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace LessonManager.Model
+{
+    internal class ActivityDB : IActivityDB
+    {
+        private DbContext? m_DbContext = null!;
+
+        public DbSet<Activity> Activities { get; set; } = null!;
+
+        public void ActivityDBInit(DbContext context)
+        {
+            m_DbContext = context;
+            Activities.Load();
+        }
+
+        public void AddActivity(string? name, Subject subject, ActivityType type, DateTime activityTime)
+        {
+            Activities.Add(
+                new Activity(name, subject, type, activityTime)
+                );
+        }
+
+        public void EditActivity(string predname, Subject predsubject, string? name, ActivityType type, DateTime activityTime, ActivityStateType state)
+        {
+            if (Activities.Any(s => s.Name == name && s.Subject == predsubject))
+            {
+                MessageBox.Show("Такой предмет в этом семестре уже существует");
+                return;
+            }
+
+            Activity a = GetActivity(predname, predsubject);
+            a.Name = name;
+            a.Type = type;
+            a.ActivityTime = activityTime;
+            a.State = state;
+            m_DbContext.SaveChanges();
+        }
+
+        public Activity GetActivity(string name, Subject subject)
+        {
+            return Activities.Where(s => s.Name == name && s.Subject == subject).Select(s => s).First();
+        }
+
+        public void RemoveActivity(string name, Subject subject)
+        {
+            Activities.Remove(
+                GetActivity(name, subject)
+            );
+            m_DbContext.SaveChanges();
+        }
+    }
+}
---
+++ LessonManager/Model/IActivityDB.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonManager.Model
+{
+    internal interface IActivityDB
+    {
+        public DbSet<Activity> Activities { get; set; }
+
+        public void ActivityDBInit(DbContext context);
+
+        public void AddActivity(string? name, Subject subject, ActivityType type, DateTime activityTime);
+
+        public void RemoveActivity(string name, Subject subject);
+
+        public void EditActivity(string predname, Subject predsubject, string? name, ActivityType type, DateTime activityTime, ActivityStateType state);
+
+        public Activity GetActivity(string name, Subject subject);
+    }
+}
