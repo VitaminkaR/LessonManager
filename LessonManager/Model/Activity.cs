71280b00f7716c5d59cae11f969eb78888ea8689
VladimirR <rvitaminka20@gmail.com>
2025-01-15 18:54:21 +0300

    Добавлена завсимость занятия от дисциплины

--- LessonManager/Model/Activity.cs
+++ LessonManager/Model/Activity.cs
@@ -27,14 +27,16 @@ namespace LessonManager.Model
     {
         public int ID { get; set; }
         public string? Name { get; set; }
+        public Subject Subject { get; set; }
         public ActivityType Type { get; set; }
         public DateTime ActivityTime { get; set; }
         public ActivityStateType State { get; set; }
 
-        public Activity(string? name, ActivityType type, DateTime activityTime, ActivityStateType state)
+        public Activity(string? name, Subject subject, ActivityType type, DateTime activityTime, ActivityStateType state)
         {
             Name = name;
             Type = type;
+            Subject = subject;
             ActivityTime = activityTime;
             State = state;
         }
