00954dcfb44870ae4bc94479db1cfa56ed3946ca
VladimirR <rvitaminka20@gmail.com>
2025-02-09 16:37:25 +0300

    Фикс высчитывания дней

--- LessonManager/View/Converters/DaysToDeadLineColorConverter.cs
+++ LessonManager/View/Converters/DaysToDeadLineColorConverter.cs
@@ -8,7 +8,9 @@ namespace LessonManager.View.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            int days = (((DateTime)value) - DateTime.Now).Days;
+            var dt1 = (DateTime)value;
+            var dt2 = DateTime.Now;
+            int days = (new DateTime(dt1.Year, dt1.Month, dt1.Day) - new DateTime(dt2.Year, dt2.Month, dt2.Day)).Days;
             return new SolidColorBrush(days > 0 ? Color.FromRgb(0, 200, 0) : (days == 0 ? Color.FromRgb(200, 200, 0) : Color.FromRgb(200, 0, 0)));
         }
 
--- LessonManager/View/Converters/DaysToDeadLineConverter.cs
+++ LessonManager/View/Converters/DaysToDeadLineConverter.cs
@@ -7,7 +7,9 @@ namespace LessonManager.View.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (((DateTime)value) - DateTime.Now).Days;
+            var dt1 = (DateTime)value;
+            var dt2 = DateTime.Now;
+            return (new DateTime(dt1.Year, dt1.Month, dt1.Day) - new DateTime(dt2.Year, dt2.Month, dt2.Day)).Days;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
