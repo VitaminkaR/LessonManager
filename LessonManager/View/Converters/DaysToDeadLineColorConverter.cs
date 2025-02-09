181be64f749d82b6442c56af9b00ae67e72f5cae
VladimirR <rvitaminka20@gmail.com>
2025-02-09 15:31:32 +0300

    Реализован показ дней до дедлайна

---
+++ LessonManager/View/Converters/DaysToDeadLineColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace LessonManager.View.Converters
+{
+    class DaysToDeadLineColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int days = (((DateTime)value) - DateTime.Now).Days;
+            return new SolidColorBrush(days > 0 ? Color.FromRgb(0, 200, 0) : (days == 0 ? Color.FromRgb(200, 200, 0) : Color.FromRgb(200, 0, 0)));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LessonManager/View/Converters/DaysToDeadLineConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace LessonManager.View.Converters
+{
+    class DaysToDeadLineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (((DateTime)value) - DateTime.Now).Days;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
